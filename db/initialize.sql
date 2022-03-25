baad9497790e67250127c7ebd277bece62649d97
sjcl <bebe@sjcl.me>
2022-03-25 19:33:04 +0900

    Add created_at

--- db/initialize.sql
+++ db/initialize.sql
@@ -4,14 +4,15 @@ GRANT INSERT,SELECT,UPDATE,DELETE ON `shionstagram_db`.* TO `shion`@`%`;
 CREATE DATABASE IF NOT EXISTS `shionstagram_db`;
 
 CREATE TABLE IF NOT EXISTS `shionstagram_db`.`posts` (
-    `id`       BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `uuid`     VARBINARY(16) NOT NULL,
-    `name`     VARCHAR(255) NOT NULL,
-    `location` VARCHAR(255) NOT NULL,
-    `message`  VARCHAR(2048) NOT NULL,
-    `img_src`  VARCHAR(255),
-    `avatar`   TINYINT UNSIGNED NOT NULL,
-    `pending`  BOOLEAN NOT NULL,
+    `id`         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `uuid`       VARBINARY(16)    NOT NULL,
+    `name`       VARCHAR(255)     NOT NULL,
+    `location`   VARCHAR(255)     NOT NULL,
+    `message`    VARCHAR(2048)    NOT NULL,
+    `img_src`    VARCHAR(255),
+    `avatar`     TINYINT UNSIGNED NOT NULL,
+    `pending`    BOOLEAN          NOT NULL,
+    `created_at` TIMESTAMP        NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     PRIMARY KEY ( `id` )
 )
