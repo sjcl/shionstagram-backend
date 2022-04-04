9fc00605831fa58e44bec0b6529bed8f914bddd0
sjcl <bebe@sjcl.me>
2022-04-05 00:38:17 +0900

    Update table name

--- db/initialize.sql
+++ db/initialize.sql
@@ -3,7 +3,7 @@ GRANT INSERT,SELECT,UPDATE,DELETE ON `shionstagram_db`.* TO `shion`@`%`;
 
 CREATE DATABASE IF NOT EXISTS `shionstagram_db`;
 
-CREATE TABLE IF NOT EXISTS `shionstagram_db`.`posts` (
+CREATE TABLE IF NOT EXISTS `shionstagram_db`.`messages` (
     `id`         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     `uuid`         VARBINARY(16)    NOT NULL,
     `twitter_name` VARCHAR(15)      NOT NULL,
