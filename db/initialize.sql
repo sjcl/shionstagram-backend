9f5ff61136fbf7e636e9b064d5f003c1470c6d9e
sjcl <bebe@sjcl.me>
2022-04-08 21:20:52 +0900

    Add discord_message_id column

--- db/initialize.sql
+++ db/initialize.sql
@@ -4,16 +4,17 @@ GRANT INSERT,SELECT,UPDATE,DELETE ON `shionstagram_db`.* TO `shion`@`%`;
 CREATE DATABASE IF NOT EXISTS `shionstagram_db`;
 
 CREATE TABLE IF NOT EXISTS `shionstagram_db`.`messages` (
-    `id`           BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `uuid`         UUID    NOT NULL,
-    `twitter_name` VARCHAR(15)      NOT NULL,
-    `name`         VARCHAR(100)     NOT NULL,
-    `location`     VARCHAR(100),
-    `message`      VARCHAR(500)     NOT NULL,
-    `image`        VARCHAR(255),
-    `avatar`       TINYINT UNSIGNED NOT NULL,
-    `is_pending`   BOOLEAN          NOT NULL DEFAULT 1,
-    `created_at`   TIMESTAMP        NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `id`                 BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `uuid`               UUID    NOT NULL,
+    `twitter_name`       VARCHAR(15)      NOT NULL,
+    `name`               VARCHAR(100)     NOT NULL,
+    `location`           VARCHAR(100),
+    `message`            VARCHAR(500)     NOT NULL,
+    `image`              VARCHAR(255),
+    `avatar`             TINYINT UNSIGNED NOT NULL,
+    `is_pending`         BOOLEAN          NOT NULL DEFAULT 1,
+    `created_at`         TIMESTAMP        NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `discord_message_id` BIGINT UNSIGNED,
 
     PRIMARY KEY ( `id` )
 )
